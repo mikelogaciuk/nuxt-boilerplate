aceea0d93ca8f4423f7f5cedbb47c042bc66844f
Michał Logaciuk <mlog00@me.pl>
2025-07-31 16:15:44 +0200

    Enable Shiki

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,32 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/app.css'],
   modules: ['@nuxt/content', '@nuxt/icon', '@nuxt/fonts', '@nuxt/eslint'],
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          // Get the themes from here: https://shiki.style/themes
+          theme: 'dracula',
+          langs: [
+            'c',
+            'cpp',
+            'java',
+            'ruby',
+            'python',
+            'elixir',
+            'json',
+            'js',
+            'ts',
+            'html',
+            'css',
+            'vue',
+            'shell',
+            'mdc',
+            'md',
+            'yaml',
+          ],
+        },
+      },
+    },
+  },
 });
