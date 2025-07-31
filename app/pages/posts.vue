45b4d96e7083b7bc8aa5d4effea265b91f148307
Michał Logaciuk <mlog00@me.pl>
2025-07-31 13:32:30 +0200

    Initial commit

---
+++ app/pages/posts.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+        <ul>
+            <li v-for="post in allPosts" :key="post.id">
+                <nuxt-link :to="post.path">{{ post.title }}</nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const allPosts = await queryCollection('blog').order('date', 'DESC').all()
+</script>
+
+<style></style>
